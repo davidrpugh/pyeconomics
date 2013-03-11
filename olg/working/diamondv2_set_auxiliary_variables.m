d414c2fc7f4014c8067a788da697298eae553825
unknown <s0899679@ECON-PGR-010.econ.ed.ac.uk>
2013-03-11 17:31:45 +0000

    Finished housekeeping for OLG models

--- olg/working/diamondv2_set_auxiliary_variables.m
+++
@@ -1,7 +0,0 @@
-function y = diamondv2_set_auxiliary_variables(y, x, params)
-%
-% Status : Computes static model for Dynare
-%
-% Warning : this file is generated automatically by Dynare
-%           from model file (.mod)
-
