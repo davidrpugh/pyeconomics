5b9e66bbc0052db15d99c6b09f8e9a630abc7a19
David R. Pugh <drobert.pugh@gmail.com>
2014-01-16 16:21:04 +0000

    initial commit for chapter 1 of the Handbook of Computational Economics.

---
+++ ace/chapter1.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+class HashProducer(object):
+    """Abstract class representing a hash producing firm."""
+    
+class BeansProducer(object):
+    """Abstract class representing a beans producing firm."""
+    
+class Consumer(object):
+    """Abstract class representing a consumer."""
+    
